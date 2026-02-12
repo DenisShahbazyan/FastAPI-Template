144765b313a88637fb4aa8348834b6f3c2612d4a
Денис Шахбазян <otbephncb@gmail.com>
2026-02-12 23:27:03 +0300

    change mypy to ty

--- app/models/mixin/_utils.py
+++ app/models/mixin/_utils.py
@@ -2,5 +2,10 @@ from datetime import datetime, timezone
 
 
 def get_current_dt_utc() -> datetime:
+    """Возвращает текущее время в UTC.
+
+    Returns:
+        datetime: Текущее время в UTC
+    """
     dt = datetime.now(tz=timezone.utc)
     return dt.replace(tzinfo=None)
--- app/models/mixin/int_id_pk.py
+++ app/models/mixin/int_id_pk.py
@@ -3,4 +3,6 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 
 class IntIdPkMixin:
+    """Миксин для добавления поля id (первичного ключа)."""
+
     id: Mapped[int] = mapped_column(BigInteger, Identity(), primary_key=True)
