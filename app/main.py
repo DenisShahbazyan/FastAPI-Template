b492be4db8500c3b226bc3acbb1fa32a8adff2e6
Денис Шахбазян <otbephncb@gmail.com>
2026-03-03 16:10:18 +0300

    del ORJSONResponse

--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
 
 from app.api.routers import main_router
 from app.core.middleware.cors import add_cors_middleware
 
 app = FastAPI(
-    default_response_class=ORJSONResponse,
     redoc_url=None,
 )
 
