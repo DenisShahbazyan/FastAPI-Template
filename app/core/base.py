51a3c2cfea0ce016efbe682e9ddebb4c4ddd46e6
Денис Шахбазян <otbephncb@gmail.com>
2026-03-07 18:56:04 +0300

    drop app/core/base.py for alembic

--- app/core/base.py
+++
@@ -1,4 +0,0 @@
-"""Импорты класса Base и всех моделей для Alembic."""
-
-from app.models._base import Base  # noqa: F401
-from app.models.user import User  # noqa: F401
--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,2 @@
+from ._base import Base  # noqa: F401
 from .user import User  # noqa: F401
